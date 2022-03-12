f9c84ec6f127e1ea6f5bc770d843126d7f4e52af
hkgnp <ben@pngs.cc>
2022-03-12 12:14:27 +0800

    fixed calculation not rendering table

--- src/index.js
+++ src/index.js
@@ -1,36 +1,36 @@
-import '@logseq/libs';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import App from './App';
-import { blocksAsColumns } from './blocksAsColumns';
-import { childBlocksAsColumns } from './childBlocksAsColumns';
+import "@logseq/libs";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import App from "./App";
+import { blocksAsColumns } from "./blocksAsColumns";
+import { childBlocksAsColumns } from "./childBlocksAsColumns";
 
 const main = async () => {
-  console.log('Table Render plugin loaded');
+    console.log("Table Render plugin loaded");
 
-  // Generate unique identifier
-  const uniqueIdentifier = () =>
-    Math.random()
-      .toString(36)
-      .replace(/[^a-z]+/g, '');
+    // Generate unique identifier
+    const uniqueIdentifier = () =>
+        Math.random()
+            .toString(36)
+            .replace(/[^a-z]+/g, "");
 
-  // Insert renderer upon slash command
-  logseq.Editor.registerSlashCommand('table render', async () => {
-    await logseq.Editor.insertAtEditingCursor(
-      `{{renderer :tables_${uniqueIdentifier()}}}`
-    );
-  });
+    // Insert renderer upon slash command
+    logseq.Editor.registerSlashCommand("table render", async () => {
+        await logseq.Editor.insertAtEditingCursor(
+            `{{renderer :tables_${uniqueIdentifier()}}}`
+        );
+    });
 
-  logseq.App.onMacroRendererSlotted(async ({ slot, payload }) => {
-    // Get uuid of payload so that child blocks can be retrieved for the board
-    const uuid = payload.uuid;
-    const [type] = payload.arguments;
-    const id = type.split('_')[1]?.trim();
-    const tableId = `tables_${id}`;
+    logseq.App.onMacroRendererSlotted(async ({ slot, payload }) => {
+        // Get uuid of payload so that child blocks can be retrieved for the board
+        const uuid = payload.uuid;
+        const [type] = payload.arguments;
+        const id = type.split("_")[1]?.trim();
+        const tableId = `tables_${id}`;
 
-    if (!type.startsWith(':tables_')) return;
+        if (!type.startsWith(":tables_")) return;
 
-    logseq.provideStyle(`
+        logseq.provideStyle(`
     .tableHeader {
       border-bottom: solid 3px red;
       background: aliceblue;
@@ -70,36 +70,41 @@ const main = async () => {
     }
     `);
 
-    const renderBlock = await logseq.Editor.getBlock(uuid, {
-      includeChildren: true,
+        const renderBlock = await logseq.Editor.getBlock(uuid, {
+            includeChildren: true,
+        });
+
+        if (renderBlock.children[0] && renderBlock.children[0].children) {
+            const blockData = renderBlock.children[0].children;
+            const summaryContent = renderBlock.children[0].content;
+
+            const { colArr, rowArr } = summaryContent.includes("rows")
+                ? await childBlocksAsColumns(blockData)
+                : await blocksAsColumns(blockData);
+
+            // Use React to render board
+            const board = ReactDOMServer.renderToStaticMarkup(
+                <App
+                    colArr={colArr}
+                    rowArr={rowArr}
+                    summaryContent={summaryContent}
+                    blockData={blockData}
+                />
+            );
+
+            // Set div for renderer to use
+            const cmBoard = (board) => {
+                return `<div id="${tableId}" data-slot-id="${slot}" data-table-id="${tableId}" data-block-uuid="${uuid}">${board}</div>`;
+            };
+
+            logseq.provideUI({
+                key: `${tableId}`,
+                slot,
+                reset: true,
+                template: cmBoard(board),
+            });
+        }
     });
-
-    if (renderBlock.children[0] && renderBlock.children[0].children) {
-      const blockData = renderBlock.children[0].children;
-      const summaryContent = renderBlock.children[0].content;
-
-      const { colArr, rowArr } = summaryContent.includes('rows')
-        ? await childBlocksAsColumns(blockData)
-        : await blocksAsColumns(blockData);
-
-      // Use React to render board
-      const board = ReactDOMServer.renderToStaticMarkup(
-        <App colArr={colArr} rowArr={rowArr} summaryContent={summaryContent} />
-      );
-
-      // Set div for renderer to use
-      const cmBoard = (board) => {
-        return `<div id="${tableId}" data-slot-id="${slot}" data-table-id="${tableId}" data-block-uuid="${uuid}">${board}</div>`;
-      };
-
-      logseq.provideUI({
-        key: `${tableId}`,
-        slot,
-        reset: true,
-        template: cmBoard(board),
-      });
-    }
-  });
 };
 
 logseq.ready(main).catch(console.error);
