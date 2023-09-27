3e6718c6fc3fae5a9fa694a6c1e852f539dd99c2
hkgnp <github.com/hkgnp>
2023-09-27 16:02:06 +0800

    fix: retain content str

--- src/helpers/handle-cell-type.tsx
+++ src/helpers/handle-cell-type.tsx
@@ -1,29 +1,35 @@
+import { ReactNode, createElement } from "react";
+import reactStringReplace from "react-string-replace";
+
 export const checkCell = (
   graphName: string,
-  str: string,
-): JSX.Element | string => {
+  content: string,
+): ReactNode | string => {
+  let str: ReactNode | string = content;
   // Check for page
   const rxPageRef = /\[\[([^)]*)\]\]/;
-  const pageName = rxPageRef.exec(str);
-
-  const rxBlockRef = /\(\(([^)]*)\)\)/;
-  const blockRef = rxBlockRef.exec(str);
-
+  const pageName = rxPageRef.exec(content);
   if (pageName) {
-    return (
+    const elem = (
       <a href={`logseq://graph/${graphName}?page=${pageName[1]}`}>
         {pageName[1]}
       </a>
     );
-  } else if (blockRef) {
-    return (
-      <a href={`logseq://graph/${graphName}?block-id=${blockRef[1]}`}>
-        {blockRef[1]}
-      </a>
-    );
-  } else {
-    return str;
+
+    str = reactStringReplace(content, pageName[0], () => elem);
   }
 
+  return str;
+
+  //  const rxBlockRef = /\(\(([^)]*)\)\)/;
+  //  const blockRef = rxBlockRef.exec(content);
+  //  if (blockRef) {
+  //    const blockContent = (await logseq.Editor.getBlock(blockRef[1])).content;
+  //    return (
+  //      <a href={`logseq://graph/${graphName}?block-id=${blockRef[1]}`}>
+  //        {blockContent}
+  //      </a>
+  //    );
+  //  }
   // Check for block
 };
